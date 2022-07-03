e4c16fa167a1fbcb1b2bd3b59293fb22a4d73ca6
Gioele Vicini <gioy00gv@gmail.com>
2022-07-03 21:47:35 +0200

    aggiornamento adminActivity.kt

---
+++ app/src/main/java/com/gv/videoteca/Film.kt
@@ -0,0 +1,20 @@
+package com.gv.videoteca
+
+class Film {
+    var name=""
+    var genre=""
+    var anno=1900
+    var description=""
+
+    constructor(name:String, genre:String, anno:Int, description:String){
+        this.name=name
+        this.genre=genre
+        this.anno=anno
+        this.description=description
+    }
+
+    constructor(){
+
+    }
+
+}
--- app/src/main/java/com/gv/videoteca/cercaFilmActivity.kt
+++ app/src/main/java/com/gv/videoteca/cercaFilmActivity.kt
@@ -1,24 +1,87 @@
 package com.gv.videoteca
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
+import android.view.View
+import android.widget.*
 
 class cercaFilmActivity : AppCompatActivity() {
+    var generi = arrayOf("Genre", "Animazione", "Avventura",
+        "Commedia", "Drammatico", "Fantascienza", "Fantasy",
+        "Giallo", "Horror", "Musical", "Storico", "Western")
+    var selectedGenre = ""
+
+    var anni = arrayOf("Year")
+    var selectedYear = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cerca_film)
-
         var search = findViewById(R.id.search) as Button
         var movie = findViewById(R.id.movie) as EditText
+
+        val genreSpinner = findViewById<Spinner>(R.id.genere) as Spinner
+        val arrayAdapterGenre = ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, generi)
+        genreSpinner.adapter = arrayAdapterGenre
+        genreSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, pos: Int, p3: Long) {
+                //Toast.makeText(applicationContext, "Selezionato " + generi[pos], Toast.LENGTH_LONG).show() troppi toast
+                selectedGenre = generi[pos]
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+        }
+
+        var y = 1930
+        while (y < 2023) {
+            anni += y.toString()
+            y++
+        }
+        val yearSpinner = findViewById<Spinner>(R.id.anno) as Spinner
+        val arrayAdapterYear = ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, anni)
+        yearSpinner.adapter = arrayAdapterYear
+        yearSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, pos: Int, p3: Long) {
+                //Toast.makeText(applicationContext, "Selezionato " + anni[pos], Toast.LENGTH_LONG).show() troppi toast
+                selectedYear = anni[pos]
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+        }
+
+
 
         search.setOnClickListener {
             var t_movie= movie.text.toString()
 
             if(!t_movie.equals("")){
                 //Todo : cercare film nel db e visualizzare i risultati
+                if(selectedYear.equals("Year") and selectedGenre.equals("Genre")){
+                    //ricerca solo per nome
+                    Toast.makeText(this, "anno e genere in def", Toast.LENGTH_SHORT).show()
+                    //startActivity(Intent(this, ResultActivity::class.java))
+                }else if(selectedGenre.equals("Genre")){
+                    //ricerca per nome e anno
+
+                    Toast.makeText(this, "genere in def", Toast.LENGTH_SHORT).show()
+                    //startActivity(Intent(this, ResultActivity::class.java))
+                }else if(selectedYear.equals("Year")){
+                    //ricerca per nome e genere
+
+
+                    Toast.makeText(this, "anno in def", Toast.LENGTH_SHORT).show()
+                    //startActivity(Intent(this, ResultActivity::class.java))
+                }else{
+                    //ricerca per nome, genere e anno
+
+
+                    //startActivity(Intent(this, ResultActivity::class.java))
+                }
             }
             else{
                 Toast.makeText(this, "Inserisci il film da cercare", Toast.LENGTH_SHORT).show()
